948c50b092947a5f338f46a55b55f929e777a570
Suraj Pratap <92919173+SurajPratap10@users.noreply.github.com>
2022-10-18 01:37:06 +0530

    Add files via upload

---
+++ Admin Dashboard UI/orders.js
@@ -0,0 +1,68 @@
+const Orders = [
+{  
+    productName: 'iPhone 14',
+    productNumber: '63474',
+    paymnetStatus: 'Paid',
+    shipping:   'Delivered'
+},
+
+{
+    productName: 'BOAT Headphones',
+    productNumber: '67698',
+    paymnetStatus: 'Due',
+    shipping:   'Pending'
+},
+
+{
+    productName: 'GPS 4K Drone',
+    productNumber: '98771',
+    paymnetStatus: 'Refunded',
+    shipping:   'Declined'
+},
+
+{
+    productName: 'Home Furniture',
+    productNumber: '74302',
+    paymnetStatus: 'Paid',
+    shipping:   'Delivered'
+},
+
+{
+    productName: 'HP Pavillion Laptop',
+    productNumber: '85671',
+    paymnetStatus: 'Due',
+    shipping:   'Pending'
+ },
+
+ {
+    productName: 'FIFA 23',
+    productNumber: '05370',
+    paymnetStatus: 'Paid',
+    shipping:   'Delivered'
+ },
+ {
+    productName: 'Sofa Set',
+    productNumber: '34370',
+    paymnetStatus: 'Paid',
+    shipping:   'Delivered'
+ },
+ {
+    productName: 'RD Sharma Book',
+    productNumber: '11342',
+    paymnetStatus: 'Due',
+    shipping:   'Pending'
+ },
+ {
+    productName: 'PS5',
+    productNumber: '56551',
+    paymnetStatus: 'Refunded',
+    shipping:   'Declined'
+ },
+ {
+    productName: 'Canvas',
+    productNumber: '89771',
+    paymnetStatus: 'Paid',
+    shipping:   'Delivered'
+ },
+
+]
